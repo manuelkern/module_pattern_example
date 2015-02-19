7a3a7dd4081dd525885742db2536ee97f5559cc6
Jay Swain <github@jayswain.com>
2015-02-19 11:49:35 +0100

    add close functionality

--- app.js
+++ app.js
@@ -9,12 +9,29 @@ App.Modal = (function(){
     modalTrigger.on("click", function(event){
       event.preventDefault();
       activateModalContent();
+      bindBackgroundClose();
     });
   };
 
   var activateModalContent = function(){
     modal.addClass("active");
     modalBackground.addClass("active");
+  };
+
+  var bindBackgroundClose = function(){
+    modalBackground.on("click", function(e){
+      deactivateModalContent();
+    });
+  };
+
+  var removeBackgroundHandler = function(){
+    modalBackground.off("click");
+  };
+
+  var deactivateModalContent = function(){
+    modal.removeClass("active");
+    modalBackground.removeClass("active");
+    removeBackgroundHandler();
   };
 
   var initialize = function(){
