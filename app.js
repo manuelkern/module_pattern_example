ea944eb1867ecc95c007937775edb4ad12a7691d
Jay Swain <github@jayswain.com>
2015-02-19 11:15:13 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var App = App || {};
+
+App.Modal = (function(){
+
+  var setupTrigger = function(){
+    console.log("fooooo");
+  };
+
+  var initialize = function(){
+    setupTrigger();
+  };
+
+
+  return {
+    initialize: initialize
+  };
+})();
